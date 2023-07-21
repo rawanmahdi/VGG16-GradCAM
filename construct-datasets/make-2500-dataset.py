8fc90402a6b6a5072e70beecb60c2bccd2c70d10
rawanmahdi <mahdir3@mcmaster.ca>
2023-07-21 10:12:00 -0400

    correct script to seperate testing and training data

--- construct-datasets/make-2500-dataset.py
+++ construct-datasets/make-2500-dataset.py
@@ -1,3 +1,4 @@
+#%%
 import os
 import shutil
 
@@ -7,9 +8,11 @@ destdir = 'C:/Users/Rawan Alamily/Downloads/McSCert Co-op/VGG16-GradCAM-data/250
 for folder in ['/Bengal', '/Persian', '/Ragdoll', '/Siamese', '/Tabby']:
     count = 0
     for img in os.listdir(datadir+folder):
-        if count<2800 :
-            shutil.move(datadir+folder+'/'+img, destdir+folder+'/'+img)
+        if count<=2000:
+            shutil.copy(datadir+folder+'/'+img, destdir+'/TRAIN'+folder+'/'+img )
+            count+=1
+        elif count<=2800:
+            shutil.copy(datadir+folder+'/'+img, destdir+'/TEST'+folder+'/'+img )
             count+=1
         else:
             break
-
--- construct-datasets/make-50-dataset.py
+++ construct-datasets/make-50-dataset.py
@@ -1,14 +1,20 @@
+#%%
 import os
 import shutil
 
-datadir = 'C:/Users/Rawan Alamily/Downloads/huge-cat-data'
-destdir = 'C:/Users/Rawan Alamily/Downloads/McSCert Co-op/VGG16-GradCAM-data/2500-cat-breed'
+datadir = 'C:/Users/Rawan Alamily/Downloads/huge-cat-data/images'
+destdir = 'C:/Users/Rawan Alamily/Downloads/McSCert Co-op/VGG16-GradCAM-data/50-cat-breeds'
 
 for folder in ['/Bengal', '/Persian', '/Ragdoll', '/Siamese', '/Tabby']:
     count = 0
     for img in os.listdir(datadir+folder):
-        if count<2800 :
-            shutil.move(datadir+folder+img, destdir+'/new'+folder+img)
+        if count<=80:
+            shutil.copy(datadir+folder+'/'+img, destdir+'/TRAIN'+folder+'/'+img )
+            count+=1
+        elif count<=100:
+            shutil.copy(datadir+folder+'/'+img, destdir+'/TEST'+folder+'/'+img )
             count+=1
         else:
             break
+
+# %%
