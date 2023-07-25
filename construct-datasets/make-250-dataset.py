766c1f5d9727d501219984ce4bed0d6837392468
rawanmahdi <mahdir3@mcmaster.ca>
2023-07-25 09:11:21 -0400

    add more scripts for building datasets

---
+++ construct-datasets/make-250-dataset.py
@@ -0,0 +1,20 @@
+#%%
+import os
+import shutil
+
+datadir = 'C:/Users/Rawan Alamily/Downloads/huge-cat-data/images'
+destdir = 'C:/Users/Rawan Alamily/Downloads/McSCert Co-op/VGG16-GradCAM-data/250-cat-breeds'
+
+for folder in ['/Bengal', '/Persian', '/Ragdoll', '/Siamese', '/Tabby']:
+    count = 0
+    for img in os.listdir(datadir+folder):
+        if count<=250:
+            shutil.copy(datadir+folder+'/'+img, destdir+'/TRAIN'+folder+'/'+img )
+            count+=1
+        elif count<=300:
+            shutil.copy(datadir+folder+'/'+img, destdir+'/TEST'+folder+'/'+img )
+            count+=1
+        else:
+            break
+
+# %%
