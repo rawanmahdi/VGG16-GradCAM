6f609e6f71ae35b2e3914508353a2cc773240738
rawanmahdi <mahdir3@mcmaster.ca>
2023-08-15 11:29:05 -0400

    testing packaged implementation cnncam

--- cnncam-test/cnncam_play.py
+++ cnncam-test/cnncam_play.py
@@ -1,60 +1,14 @@
 #%%
+from cnncam import GradCAM
+import numpy as np
+from keras.applications.vgg16 import VGG16
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
-from keras.applications.vgg16 import VGG16
-import numpy as np
-import cv2
 import matplotlib.pyplot as plt
+import cv2
 from mpl_toolkits.axes_grid1 import ImageGrid
+
 #%%
-class GradCAM:
-  def __init__(self, model, classIdx, layerName=None):
-    self.model = model
-    self.classIdx = classIdx
-    self.layerName = layerName
-
-    if self.layerName is None:
-      for layer in (self.model.layers):
-        if len(layer.output_shape) == 4:
-          self.layerName = layer.name
-        else:
-          raise ValueError("Could not find 4D layer, GradCAM cannot be applied")
-  
-
-  def get_gradcam_heatmap(self, img, eps=1e-8):
-    # add dim to img
-    img = np.expand_dims(img, axis=0)
-
-    # build model that maps input image to activations of conv layer of model and predicted output
-    grad_model = tf.keras.models.Model(
-        inputs=self.model.inputs, # img inputs
-        outputs=[self.model.get_layer(self.layerName).output, self.model.output] 
-    )
-
-    # compute gradient of top predicted class for img with respect to activation of last convolutional layer
-    with tf.GradientTape() as tape:
-      conv_output, preds = grad_model(img)
-      loss = preds[:, self.classIdx]
-    grads = tape.gradient(loss, conv_output) # gradient of output neuron (predicted class)
-
-    # vector of mean intensity of gradient over specified feature map channel
-    pooled_grads = tf.reduce_mean(grads, axis=(0,1,2))
-
-    # get heatmap by summing all the channels after multiplying feature map by feature importance for top predicted class
-    conv_output = conv_output[0]
-    heatmap = conv_output @ pooled_grads[..., tf.newaxis]
-    heatmap = tf.squeeze(heatmap).numpy()
-
-    # normalize heatmap
-    numer = heatmap - np.min(heatmap)
-    denom = (heatmap.max() - heatmap.min()) + eps
-    heatmap = numer / denom
-    heatmap = (heatmap * 255).astype("uint8")
-
-    return heatmap
-  
-
-
 def build_heatmap(model, img, predictedClass, layerName):
   # get heatmap
   heatmapBuilder = GradCAM(model, predictedClass, layerName)
@@ -86,7 +40,7 @@ maps1 = []
 preds1 = []
 maps2 = []
 #%%
-num_samples = 7
+num_samples = 1
 for i in range(num_samples):
   img, label = train_data.next()
   for model_name in ['/overfitted/400-noise-40dropout-93-32','/overfitted/400-noise-40dropout-7layer-98-30', 
@@ -117,3 +71,16 @@ for i in range(num_samples*7):
   grid[i].set_title(f"Predicted {labels.get(preds1[i])}")
 
 plt.show()
+#%%
+maps = []
+for i in range(num_samples):
+  img, label = train_data.next()
+  for model_name in ['/overfitted/400-noise-40dropout-93-32','/overfitted/400-noise-40dropout-7layer-98-30', 
+                     '/overfitted/400-sample-7layer-100-52', '/overfitted/400-sample-7layer-dropout-82-52', 
+                     '/overfitted/400-sample-256bs-75dropout-88-86',
+                    '/overfitted/400-sample-256bs-100-85', '/overfitted/400-sample-dropout-98-80']:
+    model = tf.keras.models.load_model(modeldir+model_name)
+    pred = np.argmax(model.predict(img))
+    heatmapBuilder = GradCAM(model, pred, layerName='block5_pool')
+    heatmap = heatmapBuilder.get_gradcam_heatmap(tf.squeeze(img))
+    maps.append(heatmap)
